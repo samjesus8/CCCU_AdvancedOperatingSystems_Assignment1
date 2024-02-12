49ce7cc6136da989ae9c984331585392df8b7908
samjesus8 <samueljesuthas27@gmail.com>
2024-02-12 13:32:49 +0000

    Added UPP.db & Adressed BYE syntax

--- main/LIFO.sh
+++ main/LIFO.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo "LIFO.sh"
+
 return 0
--- main/Menu.sh
+++ main/Menu.sh
@@ -3,10 +3,10 @@
 #Menu Display & Select
 Menu()
 {
-	echo "Make your selection or type bye to exit:" 
-	echo "1 for FIFO"
-	echo "2 for LIFO"
-	echo "BYE for exit"
+	echo "\033[37;44mMake your selection or type bye to exit:\033[0m"
+	echo "\033[32m1 for FIFO\033[0m"
+	echo "\033[32m2 for LIFO\033[0m"
+	echo "\033[31mBYE for exit\033[0m"
 	echo "Please Enter Selection:"
 	read Sel
 	MenuSel $Sel
@@ -15,11 +15,17 @@ Menu()
 #Menu case
 MenuSel()
 {
-	case $1 in
+	uppercase_input=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+	
+	case $uppercase_input in
 		1) sh FIFO.sh;;
-			2) sh LIFO.sh;;
+		
+		2) sh LIFO.sh;;
+		
 		BYE) exit;;
+
 		*) echo "Invalid Selection"
+		
 		sleep 1
 		Menu;;
 	esac
