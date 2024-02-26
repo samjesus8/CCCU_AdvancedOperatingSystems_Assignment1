bfd0018ca84368ed5806d789a0303fe1bd60aea9
Samuel Jesuthas <samueljesuthas27@gmail.com>
2024-02-26 23:48:45 +0000

    [PROGRESS] Implemented valid User Creation

--- main/Admin.sh
+++ main/Admin.sh
@@ -24,17 +24,40 @@ MenuSelect(){
 
 # User Creation
 UserCreate(){
-    # Prompt for username
-    echo "Please enter Username: "
-    read username
+    while true; do
+        # Prompt for username
+        echo "Please enter Username: "
+        read username
 
-    # Prompt for password and store it
-    echo "Please enter password: "
-    read password
+        while true; do
+            # Prompt for password and store it
+            echo "Please enter password: "
+            read password
 
-    # Prompt for PIN and store it
-    echo "Please enter PIN: "
-    read PIN
+            if [ ${#password} -eq 5 ]; then
+                echo "Password length is 5."
+                break
+            else
+                echo "Password must be exactly 5 characters long!"
+                continue
+            fi
+        done
+
+        while true; do
+            # Prompt for PIN and store it
+            echo "Please enter PIN: "
+            read PIN
+            
+            if [[ $PIN =~ ^[0-9]{3}$ ]]; then
+                echo "PIN length is 3."
+                break
+            else
+                echo "PIN must be exactly 3 digits and must contain valid integers!"
+                continue
+            fi
+        done
+        break
+    done
 
     # Check if the file exists, if not, create it
     if [ ! -f "UPP.txt" ]; then
